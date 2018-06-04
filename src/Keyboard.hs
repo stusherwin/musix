b313eff4b1e774beb9cc18459b507d5c4c9fe5e3
Stu Sherwin <stu.sherwin@gmail.com>
2018-06-04 15:15:25 +0100

    Refactor keyboard

--- src/Keyboard.hs
+++ src/Keyboard.hs
@@ -9,6 +9,7 @@ module Keyboard (
 ) where
 
 import Graphics
+import Data.List (zip4)
 import qualified Data.Map.Lazy as M
 import Data.Map.Lazy ((!))
 import Chords
@@ -39,87 +40,82 @@ notes (Keyboard _ keyMap) = map (toNote . fst) $ filter (\(k, p) -> playing p) (
 playing Playing = True
 playing _ = False
 
-data WhiteKeyType = FullKey
-                  | LeftKey
-                  | MiddleKey
-                  | RightKey deriving (Show)
+data WhiteKeyType = L
+                  | M
+                  | R deriving (Show)
 
-data Key = White WhiteKeyType
-         | Black deriving (Show)
+data Key = W WhiteKeyType
+         | B deriving (Show)
 
 keyboardLayout :: M.Map Int Keyboard.Key
-keyboardLayout = M.fromList $ zip [0..] [ White LeftKey
-                                        , Black
-                                        , White MiddleKey
-                                        , Black
-                                        , White RightKey
-                                        , White LeftKey
-                                        , Black
-                                        , White MiddleKey
-                                        , Black
-                                        , White MiddleKey
-                                        , Black
-                                        , White RightKey ]
+keyboardLayout = M.fromList $ zip [0..] [ W L
+                                        , B
+                                        , W M
+                                        , B
+                                        , W R
+                                        , W L
+                                        , B
+                                        , W M
+                                        , B
+                                        , W M
+                                        , B
+                                        , W R
+                                        ]
 
-isWhite (White _) = True
-isWhite _ = False
-
-isBlack Black = True
-isBlack _ = False
-
-blackSize = V2 16 76
-whiteSize = V2 26 116
+blackKeySize = V2 16 76
+whiteKeySize = V2 26 116
 gap = 2
-blackCol = makeGColor 0.5 0.5 0.5
-whiteCol = makeGColor 1 1 1
-redCol = makeGColor 1 0 0
-blueCol = makeGColor 0 0 1
+
+black = makeGColor 0.5 0.5 0.5
+white = makeGColor 1 1 1
+red = makeGColor 1 0 0
+blue = makeGColor 0 0 1
   
 instance Drawable Keyboard where
   draw keyboard@(Keyboard scale keyMap) origin (V2 w h) = do
-    mapM_ drawKey $ M.assocs keyData
+    mapM_ drawKey keyData
     where
-      keyData = M.fromList $ zip keys $ zip3 (M.elems keyMap) keyTypes allowed
+      keyData = zip4 keys (M.elems keyMap) keyTypes allowed
       keys = M.keys keyMap
       keyTypes = map (\k -> keyboardLayout ! (k `mod` 12)) keys
       allowed = maybe (repeat True) (\s -> map (inScale s . toNote) keys) scale
-      noOfWhites key = fromIntegral $ M.size $ (M.filter (\(_, kt, _) -> isWhite kt)) $ fst $ M.split key keyData
-      drawKey :: (Int, (PlayState, Key, Bool)) -> IO ()
-      drawKey (key, (playState, keyType, allowed)) = do
+      noOfWhites key = fromIntegral $ ((key `div` 12) * 7) + ([0,1,1,2,2,3,4,4,5,5,6,6] !! (key `mod` 12))
+      drawKey :: (Int, PlayState, Key, Bool) -> IO ()
+      drawKey (key, playState, keyType, allowed) = do
         case (keyType, (key == firstKey keyboard), (key == lastKey keyboard)) of
-          (White LeftKey, _, False) -> whiteLeft
-          (White LeftKey, _, True) -> whiteFull
-          (White MiddleKey, False, False) -> whiteMiddle
-          (White MiddleKey, True, _) -> whiteLeft
-          (White MiddleKey, _, True) -> whiteRight
-          (White RightKey, False, _) -> whiteRight
-          (White RightKey, True, _) -> whiteFull
-          (Black, _, _) -> black
+          (W L, _, False) -> drawWhiteKeyLeft
+          (W L, _, True) -> drawWhiteKeyFull
+          (W M, False, False) -> drawWhiteKeyMiddle
+          (W M, True, _) -> drawWhiteKeyLeft
+          (W M, _, True) -> drawWhiteKeyRight
+          (W R, False, _) -> drawWhiteKeyRight
+          (W R, True, _) -> drawWhiteKeyFull
+          (B, _, _) -> drawBlackKey
         where
-          keyboardWidth = ((fromIntegral $ M.size keyMap) * 7.0 / 12.0) * (x whiteSize + gap)
-          keyboardHeight = y whiteSize
+          keyboardWidth = ((fromIntegral $ M.size keyMap) * 7.0 / 12.0) * (x whiteKeySize + gap)
+          keyboardHeight = y whiteKeySize
           keyboardScale = V2 (w / keyboardWidth) (h / keyboardHeight)
           keyColor = case (playState, allowed, keyType) of
-                       (Playing, False, _) -> redCol
-                       (Playing, True, _) -> blueCol
-                       (_, _, White _) -> whiteCol
-                       (_, _, Black) -> blackCol
-          whiteLeft = do
-            whiteBase
-            drawRect' whiteOffset (V2 0 0) (V2 (x whiteSize - x blackInset) (y blackSize))
-          whiteMiddle = do
-            whiteBase
-            drawRect' whiteOffset (V2 (x blackInset) 0) (V2 (x whiteSize - x blackInset) (y blackSize))
-          whiteRight = do
-            whiteBase
-            drawRect' whiteOffset (V2 (x blackInset) 0) (V2 (x whiteSize) (y blackSize))
-          whiteFull = do
-            drawRect' whiteOffset (V2 0 0) whiteSize
-          black = do
-            drawRect' blackOffset (V2 0 0) blackSize
-          whiteBase = drawRect' (whiteOffset |+| V2 0 (y blackSize)) (V2 0 0) (V2 (x whiteSize) (y blackInset))
-          blackInset = V2 ((x blackSize + gap) / 2) (y whiteSize - y blackSize)
-          whiteOffset = V2 (noOfWhites key * (x whiteSize + gap)) 0
-          blackOffset = whiteOffset |+| V2 (-(x blackInset)) (-gap)--(y blackInset + gap)
+                       (Playing, False, _) -> red
+                       (Playing, True, _) -> blue
+                       (_, _, W _) -> white
+                       (_, _, B) -> black
+          drawWhiteKeyLeft = do
+            drawWhiteBase
+            drawRect' whiteKeyOffset (V2 0 0) (V2 (x whiteKeySize - x blackKeyInset) (y blackKeySize))
+          drawWhiteKeyMiddle = do
+            drawWhiteBase
+            drawRect' whiteKeyOffset (V2 (x blackKeyInset) 0) (V2 (x whiteKeySize - x blackKeyInset) (y blackKeySize))
+          drawWhiteKeyRight = do
+            drawWhiteBase
+            drawRect' whiteKeyOffset (V2 (x blackKeyInset) 0) (V2 (x whiteKeySize) (y blackKeySize))
+          drawWhiteKeyFull = do
+            drawRect' whiteKeyOffset (V2 0 0) whiteKeySize
+          drawBlackKey = do
+            drawRect' blackKeyOffset (V2 0 0) blackKeySize
+          drawWhiteBase = drawRect' (whiteKeyOffset |+| V2 0 (y blackKeySize)) (V2 0 0) (V2 (x whiteKeySize) (y blackKeyInset))
+          blackKeyInset = V2 ((x blackKeySize + gap) / 2) (y whiteKeySize - y blackKeySize)
+          whiteKeyOffset = V2 (noOfWhites key * (x whiteKeySize + gap)) 0
+          blackKeyOffset = whiteKeyOffset |+| V2 (-(x blackKeyInset)) (-gap)
           drawRect' offset p1 p2 =
             drawRect keyColor (origin |+| (offset |**| keyboardScale)) (p1 |**| keyboardScale) (p2 |**| keyboardScale)
